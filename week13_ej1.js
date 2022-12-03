988cc511f73fab5573896ab40dcec7667562dce4
NataliCDMX <lili9106@gmail.com>
2022-12-04 02:35:29 +0300

    add project

---
+++ week13_ej1.js
@@ -0,0 +1,6 @@
+function hello (){
+
+let person = prompt(`Как Ваше имя?`);
+alert( `Приветик, ${person}!` ); 
+}
+
---
+++ week13_ej2.js
@@ -0,0 +1,27 @@
+function plus (){
+
+    let a = prompt(`Отлично! Введи первое число`);
+    let b = prompt(`Введи второе число`);
+    alert( Number (a) + Number (b) ); 
+}
+
+ function minus (){
+
+    let a = prompt(`Отлично! Введи первое число`);
+    let b = prompt(`Введи второе число`);
+    alert( Number (a) - Number (b) ); 
+}
+
+function multiply (){
+
+    let a = prompt(`Отлично! Введи первое число`);
+    let b = prompt(`Введи первое число`);
+    alert( Number (a) * Number (b) ); 
+}  
+
+function divide (){
+
+    let a = prompt(`Отлично! Введи первое число`);
+    let b = prompt(`Введи второе число`);
+    alert( Number (a) / Number (b) ); 
+}
